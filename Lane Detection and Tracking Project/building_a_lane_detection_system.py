8608bc9ad9f648c5f031cc75af1a0a50e6c18399
Hussain-D <dudekulahussain86@gmail.com>
2023-01-11 17:36:34 +0530

    On Image!

--- Lane Detection and Tracking Project/building_a_lane_detection_system.py
+++ Lane Detection and Tracking Project/building_a_lane_detection_system.py
@@ -2,21 +2,36 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-#CONVERT INTO GREY SCALE IMAGE
-def grey(image):
-    image=np.asarray(image)
-    return cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
-
-#GAUSSIAN BLUR TO REDUCE NOISE AND SMOOTHEN THE IMAGE
-def gauss(image):
-    return cv2.GaussianBlur(image,(5,5),0)
-
-#CANNY EDGE DETECTION
 def canny(image):
-    edges = cv2.Canny(image,50,150)
-    return edges
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 200, 300)
+    return canny
 
-#IMPORTING IMAGE
-img = cv2.imread("C:/Users/D P Balaiah/Documents/Sem 7/Major Project/Hu/sample lane image.jpg")
-grey_img = grey(img)
-print(grey_img)
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1), (x2,y2), (0, 255, 0), 10)
+    return line_image
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+        [(0,height-300),(4000,height-300),(2100,1550)]
+        ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+    
+image = cv2.imread("sample lane image.jpg")
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,150,np.array([]),minLineLength=10,maxLineGap=20)
+line_image = display_lines(lane_image,lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+plt.imshow(combo_image)
+plt.show()
