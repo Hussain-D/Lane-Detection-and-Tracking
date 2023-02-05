4da02234fa135401537646cfa08318cfa0dc7c70
Hussain-D <dudekulahussain86@gmail.com>
2023-02-05 10:52:31 +0530

    optimization and decoration

--- Lane Detection and Tracking Project/building_a_lane_detection_system.py
+++ Lane Detection and Tracking Project/building_a_lane_detection_system.py
@@ -25,13 +25,40 @@ def region_of_interest(image):                      # Function for ROI
     cv2.fillPoly(mask,polygons,255)                 # Filling the polygon mask with 255's(11111111)/White
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
-    
+
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope<0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+        
 image = cv2.imread("sample lane image.jpg")
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,150,np.array([]),minLineLength=10,maxLineGap=20) # Finding imp lines
-line_image = display_lines(lane_image,lines)        # Display on the image
+averaged_lines = average_slope_intercept(lane_image,lines)
+line_image = display_lines(lane_image,averaged_lines)        # Display on the image
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1) # Adding weight to the lines on the img
 plt.imshow(combo_image)
 plt.show()
